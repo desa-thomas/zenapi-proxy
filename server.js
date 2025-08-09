4261b47d2e1609bff3f351920bfc6339bd376a47
tom <desa2thomas@gmail.com>
2025-08-09 14:32:56 -0400

    added caching of quote of day

--- server.js
+++ server.js
@@ -10,8 +10,8 @@
 const express = require("express");
 const cors = require("cors");
 const os = require("os");
-const https = require("https"); 
-const fs = require("fs"); 
+const https = require("https");
+const fs = require("fs");
 
 const app = express();
 const PORT = 1984;
@@ -20,6 +20,9 @@ const FRONTEND_HOSTS = [
   "https://wisewords.tdesa.dev",
   "http://tdesa.duckdns.org:5500",
 ];
+
+let quoteofday = null;
+let cache_timestamp = null;
 
 //API endpoints
 const getquotesURL = "https://zenquotes.io/api/quotes";
@@ -79,38 +82,77 @@ app.get("/api/quotes", async (req, res) => {
  * Get quote of day proxy route
  */
 app.get("/api/quoteofday", async (req, res) => {
-  try {
-    const response = await fetch(quoteofdayURL);
-    const data = await response.json();
 
-    if (!response.ok) {
-      return res.status(response.status).json(data);
-    }
-    // res.setHeader("Access-Control-Allow-Origin", FRONTEND_HOST);
-    res.json(data);
-  } catch (error) {
-    console.error("Error fetchinng quote of day", error);
-    res.status(500).json({ error: "Failed ot fetch quote of day" });
+  //if quote of day cache exists and is still valid
+  if(quoteofday && !not_today(cache_timestamp)){
+    res.json(quoteofday); 
+    console.log("quote of day retrieved from cache")
   }
+
+  else {
+    try {
+      const response = await fetch(quoteofdayURL);
+      const data = await response.json();
+
+      if (!response.ok) {
+        return res.status(response.status).json(data);
+      }
+      
+      cache_timestamp = Date();
+      quoteofday = data; 
+      console.log(`Cache updated: ${cache_timestamp}`)
+
+      res.json(data);
+    } catch (error) {
+      console.error("Error fetchinng quote of day", error);
+      res.status(500).json({ error: "Failed ot fetch quote of day" });
+    }
+  }
+  return 
 });
+
+/**
+ * Check if timestamp is not today (calendar date)
+ *
+ * @export
+ * @param {Date} timestamp
+ * @return {boolean}
+ */
+function not_today(timestamp) {
+  if (timestamp == null) {
+    console.error("notToday: Timestamp cannot be null");
+    return false;
+  }
+  const inputDate = new Date(timestamp);
+  const today = new Date();
+
+  return (
+    inputDate.getFullYear() !== today.getFullYear() ||
+    inputDate.getMonth() !== today.getMonth() ||
+    inputDate.getDate() !== today.getDate()
+  );
+}
 
 
 //load certification if running on my server
 const hostname = os.hostname();
 if (hostname.includes("pi")) {
   const options = {
-  key: fs.readFileSync('/etc/letsencrypt/archive/tomsapi.duckdns.org/privkey1.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/archive/tomsapi.duckdns.org/fullchain1.pem'),
-};
+    key: fs.readFileSync(
+      "/etc/letsencrypt/archive/tomsapi.duckdns.org/privkey1.pem"
+    ),
+    cert: fs.readFileSync(
+      "/etc/letsencrypt/archive/tomsapi.duckdns.org/fullchain1.pem"
+    ),
+  };
 
-https.createServer(options, app).listen(PORT, ()=>{
-  console.log(`Proxy server running on port ${PORT} (HTTPS)`)
-})
+  https.createServer(options, app).listen(PORT, () => {
+    console.log(`Proxy server running on port ${PORT} (HTTPS)`);
+  });
 }
 //development server
-else{
+else {
   app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Proxy server running on port ${PORT}`);
-});
-
+    console.log(`Proxy server running on port ${PORT}`);
+  });
 }
