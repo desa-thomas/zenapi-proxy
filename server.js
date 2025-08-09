409e920b4e11b4212ab16dea3cc8e7b5a9a9fe94
tom <desa2thomas@gmail.com>
2025-08-09 12:44:51 -0400

    added https support to fix mixed content error

--- server.js
+++ server.js
@@ -8,25 +8,35 @@
  */
 
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
+const os = require("os");
+const https = require("https"); 
+const fs = require("fs"); 
 
 const app = express();
-const PORT = 1984; 
-const FRONTEND_HOSTS = ["http://localhost:5500", "https://words-of-wonder.tdesa.dev","http://words-of-wonder.tdesa.dev",  "http://tdesa.duckdns.org:5500"]; 
+const PORT = 1984;
+const FRONTEND_HOSTS = [
+  "http://localhost:5500",
+  "https://words-of-wonder.tdesa.dev",
+  "http://words-of-wonder.tdesa.dev",
+  "http://tdesa.duckdns.org:5500",
+];
 
 //API endpoints
 const getquotesURL = "https://zenquotes.io/api/quotes";
 const quoteofdayURL = "https://zenquotes.io/api/today";
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || FRONTEND_HOSTS.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  }
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || FRONTEND_HOSTS.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 
 //logger for incoming requests
 app.use((req, res, next) => {
@@ -38,7 +48,7 @@ app.use((req, res, next) => {
  * logger for response to requests
  */
 app.use((req, res, next) => {
-  res.on('finish', () => {
+  res.on("finish", () => {
     console.log(`[${res.statusCode}] ${req.method} ${req.originalUrl} `);
   });
   next();
@@ -54,14 +64,12 @@ app.get("/api/quotes", async (req, res) => {
 
     //If server response is not okay, forward error
     if (!response.ok) {
-
       return res.status(response.status).json(data);
     }
 
     //Only allow cors requests from my frontend host (local host, or my github pages)
     // res.setHeader("Access-Control-Allow-Origin", FRONTEND_HOST);
     res.json(data);
-
   } catch (error) {
     console.error("Error fetching quote:", error);
     res.status(500).json({ error: "Failed to fetch quote" });
@@ -74,21 +82,36 @@ app.get("/api/quotes", async (req, res) => {
 app.get("/api/quoteofday", async (req, res) => {
   try {
     const response = await fetch(quoteofdayURL);
-    const data = await response.json(); 
+    const data = await response.json();
 
-    if(!response.ok){
-        
-        return res.status(response.status).json(data); 
+    if (!response.ok) {
+      return res.status(response.status).json(data);
     }
     // res.setHeader("Access-Control-Allow-Origin", FRONTEND_HOST);
-    res.json(data); 
-
+    res.json(data);
   } catch (error) {
     console.error("Error fetchinng quote of day", error);
     res.status(500).json({ error: "Failed ot fetch quote of day" });
   }
 });
 
-app.listen(PORT, "0.0.0.0", () => {
+
+//load certification if running on my server
+const hostname = os.hostname();
+if (hostname == "hostpi") {
+  const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/tomsapi.duckdns.org/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/tomsapi.duckdns.org/fullchain.pem')
+};
+
+https.createServer(options, app).listen(443, ()=>{
+  console.log('Proxy server running on port 443 (HTTPS')
+})
+}
+//development server
+else{
+  app.listen(PORT, "0.0.0.0", () => {
   console.log(`Proxy server running on port ${PORT}`);
 });
+
+}
